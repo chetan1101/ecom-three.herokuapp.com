c90c9fde5493ab4c7e859ee85cb2e4c8a961b64d
chetan soni <chetan.soni@onsumaye.com>
2021-02-13 18:43:24 +0530

    update

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ app.get('/', (req, res)=> {
     res.send("server is ready");
 });
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+  }
+
 require('./db');
 app.listen(Port, () => console.log(`server is running on port ${Port}`));

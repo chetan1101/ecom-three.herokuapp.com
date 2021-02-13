d924523231dc95f84c58e90e64d98b9eb6c3d4f7
chetan soni <chetan.soni@onsumaye.com>
2021-02-13 18:57:25 +0530

    update

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ const productRouter = require('./Routes/productRoute');
 const userRouter = require('./Routes/userRoute');
 const orderRouter = require('./Routes/orderRoute');
 
+require('./db');
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-
-app.get('/', (req, res)=> {
-    res.send("server is ready");
-});
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
@@ -27,5 +25,5 @@ if(process.env.NODE_ENV === 'production'){
     })
   }
 
-require('./db');
+
 app.listen(Port, () => console.log(`server is running on port ${Port}`));
